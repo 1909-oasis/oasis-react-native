c6e7e7d77d05f8c4ae5f3e15491f667ed156a44d
maxipopuliz <maxipopuliz@gmail.com>
2019-11-27 21:57:23 -0500

    working apollo client

--- App.js
+++ App.js
@@ -1,13 +1,46 @@
-import { AppLoading } from 'expo';
-import { Asset } from 'expo-asset';
-import * as Font from 'expo-font';
-import React, { useState } from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import { AppLoading } from "expo";
+import { Asset } from "expo-asset";
+import * as Font from "expo-font";
+import React, { useState } from "react";
+import { Platform, StatusBar, StyleSheet, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
-import AppNavigator from './navigation/AppNavigator';
+//this is where apollo starts
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+
+import AppNavigator from "./navigation/AppNavigator";
 
 export default function App(props) {
+  //Apollo Client
+  const httpLink = createHttpLink({
+    uri: "https://oasis-1909-804e350939.herokuapp.com/oasis-server-v3/dev"
+  });
+  const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+  });
+
+  client
+    .query({
+      query: gql`
+        {
+          cocktails {
+            id
+          }
+        }
+      `
+    })
+    .then(response =>
+      response.data.cocktails.map(element => {
+        console.log(element.id);
+      })
+    );
+
   const [isLoadingComplete, setLoadingComplete] = useState(false);
 
   if (!isLoadingComplete && !props.skipLoadingScreen) {
@@ -21,7 +54,7 @@ export default function App(props) {
   } else {
     return (
       <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+        {Platform.OS === "ios" && <StatusBar barStyle="default" />}
         <AppNavigator />
       </View>
     );
@@ -31,16 +64,16 @@ export default function App(props) {
 async function loadResourcesAsync() {
   await Promise.all([
     Asset.loadAsync([
-      require('./assets/images/robot-dev.png'),
-      require('./assets/images/robot-prod.png'),
+      require("./assets/images/robot-dev.png"),
+      require("./assets/images/robot-prod.png")
     ]),
     Font.loadAsync({
       // This is the font that we are using for our tab bar
       ...Ionicons.font,
       // We include SpaceMono because we use it in HomeScreen.js. Feel free to
       // remove this if you are not using it in your app
-      'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-    }),
+      "space-mono": require("./assets/fonts/SpaceMono-Regular.ttf")
+    })
   ]);
 }
 
@@ -57,6 +90,6 @@ function handleFinishLoading(setLoadingComplete) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-  },
+    backgroundColor: "#fff"
+  }
 });
