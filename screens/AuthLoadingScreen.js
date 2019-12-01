e9c094b290cb46e90c5d28174f5887e1e045400a
Alisa Chan <alisachan.ny@gmail.com>
2019-11-30 21:09:29 -0500

    login/signup

--- screens/AuthLoadingScreen.js
+++ screens/AuthLoadingScreen.js
@@ -7,18 +7,56 @@ import {
   View
 } from "react-native";
 
-class AuthLoadingScreen extends React.Component {
-  componentDidMount() {
-    this._bootstrapAsync();
-  }
+export const USER_TOKEN = "auth-demo-key";
 
-  // Fetch the token from storage then navigate to our appropriate place
-  _bootstrapAsync = async () => {
-    const userToken = await AsyncStorage.getItem("userToken");
+export const onSignIn = () => AsyncStorage.setItem(USER_TOKEN, "true");
+
+export const onSignOut = () => AsyncStorage.removeItem(USER_TOKEN);
+
+export const isSignedIn = () => {
+  return async (resolve, reject) => {
+    const res = await AsyncStorage.getItem(USER_TOKEN);
+    try {
+      if (res !== null) {
+        this.props.navigation.navigate("Main");
+        resolve(true);
+      } else {
+        this.props.navigation.navigate("Auth");
+        resolve(false);
+      }
+    } catch (error) {
+      reject(error);
+    }
+  };
+};
+
+class AuthLoadingScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signedIn: false, // Determines if you are sent to Main or to Auth
+      checkedSignIn: false
+    };
+  }
+  componentDidMount() {
+    // this._bootstrapAsync();
+    isSignedIn();
+    try {
+      async res => await this.setState({ signedIn: res, checkedSignIn: true });
+    } catch (error) {
+      alert("An error has occured");
+    }
+    //   }
+
+    // Fetch the token from storage then navigate to our appropriate place
+    //   _bootstrapAsync = async () => {
+    // const userToken = await AsyncStorage.getItem(email);
+    // console.log("--------", userToken);
 
     // This will switch to the Main screen or the Auth screen and this loading screen will be unmounted and thrown away.
-    this.props.navigation.navigate(/*userToken ? "Main" : */ "Auth");
-  };
+    // Commented out the first part of the ternary
+    this.props.navigation.navigate(this.state.signedIn ? "Main" : "Auth");
+  }
 
   render() {
     return (
--- screens/LogInScreen.js
+++ screens/LogInScreen.js
@@ -2,9 +2,40 @@ import React from "react";
 import { AsyncStorage, Button, Text, TextInput, View } from "react-native";
 
 export default class LogIn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      login: false,
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: ""
+    };
+    // this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    // event.preventDefault();
+    this._signInAsync(this.state.email, this.state.password);
+    // this.setState({
+    //   firstName: "",
+    //   lastName: "",
+    //   email: "",
+    //   password: ""
+    // });
+  }
+
   static navigationOptions = {
-    title: "Log In"
+    title: "Log In / Sign Up"
   };
+
   render() {
     return (
       <View
@@ -14,8 +45,36 @@ export default class LogIn extends React.Component {
           alignItems: "center"
         }}
       >
-        <Text>Log In</Text>
-        <Text>email:</Text>
+        {/* Ternary to display Name fields if this is a Sign Up vs. Log In */}
+        {!this.state.login && [
+          <Text>First Name:</Text>,
+          <TextInput
+            style={{
+              height: 40,
+              width: 100,
+              borderBottomWidth: 1,
+              borderBottomColor: "#000000"
+            }}
+            type="text"
+            name="firstName"
+            value={this.state.firstName}
+            // onChange={this.handleChange}
+          />,
+          <Text>Last Name:</Text>,
+          <TextInput
+            style={{
+              height: 40,
+              width: 100,
+              borderBottomWidth: 1,
+              borderBottomColor: "#000000"
+            }}
+            type="text"
+            name="lastName"
+            value={this.state.lastName}
+            onChange={this.handleChange}
+          />
+        ]}
+        <Text>Email:</Text>
         <TextInput
           style={{
             height: 40,
@@ -23,8 +82,12 @@ export default class LogIn extends React.Component {
             borderBottomWidth: 1,
             borderBottomColor: "#000000"
           }}
+          type="text"
+          name="email"
+          value={this.state.email}
+          onChange={this.handleChange}
         />
-        <Text>password:</Text>
+        <Text>Password:</Text>
         <TextInput
           style={{
             height: 40,
@@ -32,16 +95,33 @@ export default class LogIn extends React.Component {
             borderBottomWidth: 1,
             borderBottomColor: "#000000"
           }}
-          // This blocks user input for a field.
+          type="text"
+          name="password"
+          value={this.state.password}
+          onChange={this.handleChange}
+          // This secures user input for a field.
           secureTextEntry={true}
         />
-        <Button title="Log In" onPress={this._signInAsync} />
+        <Button
+          title={this.state.login ? "Log In" : "Sign Up"}
+          onPress={this.handleSubmit}
+        />
+        {!this.state.login && [
+          <Text>Already have an account?</Text>,
+          <Button
+            title="Log In"
+            onPress={() => {
+              this.setState({ login: !login });
+            }}
+          />
+        ]}
       </View>
     );
   }
   // This is dummy information that lets you get past the log in screen for now.
-  // _signInAsync = async () => {
-  //   await AsyncStorage.setItem("userToken", "abc");
-  //   this.props.navigation.navigate("App");
-  // };
+  _signInAsync = async (email, password) => {
+    await AsyncStorage.setItem("email", email);
+    await AsyncStorage.setItem("password", password);
+    this.props.navigation.navigate("Main");
+  };
 }
